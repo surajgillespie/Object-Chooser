893efdde5c541e52065fe55c712c0c8363895597
Suraj <suraj.gillespie@gmail.com>
2013-09-11 23:16:19 +0530

    Remove unnecessary comments and run js beautify

--- js/activity.js
+++ js/activity.js
@@ -17,7 +17,7 @@ define(function (require) {
         //to save and resume the contents from datastore.
 
         var datastoreObject = activity.getDatastoreObject();
-        
+
         console.log(datastoreObject);
         console.log(datastoreObject);
 
@@ -28,48 +28,32 @@ define(function (require) {
             datastoreObject.save(function () {});
         };
 
-        
-            markdownParsing();
+        markdownParsing();
 
         datastoreObject.loadAsText(function (error, metadata, data) {
-            console.log(metadata);
-            //console.log(data);
             markdowntext = JSON.parse(data);
-
-            //console.log(markdowntext);
-
             inputTextContent.value = markdowntext;
-            markdownParsing();
-
-            //markdownParsing();//it has to parse only after the input text box has been loaded
+            markdownParsing(); //it has to parse only after the input text box has been loaded
         });
-        
-        function markdownParsing()
-        {
+
+        function markdownParsing() {
             var converter2 = new Markdown.Converter();
             var help = function () {
-            alert("Do you need help?");
+                alert("Do you need help?");
             }
             var options = {
-            helpButton: {
-                handler: help
-            },
-            strings: {
-                quoteexample: "whatever you're quoting, put it right here"
-            }
-        };
-        var editor2 = new Markdown.Editor(converter2, "-second", options);
+                helpButton: {
+                    handler: help
+                },
+                strings: {
+                    quoteexample: "whatever you're quoting, put it right here"
+                }
+            };
+            var editor2 = new Markdown.Editor(converter2, "-second", options);
 
-        editor2.run();
+            editor2.run();
 
         }
-        
-        
-
-        
-
-        
-
     });
 
 });
